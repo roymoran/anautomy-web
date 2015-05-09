9019a8a7ac40ca2eaf3fa8b4954e692bbe5620e9
Roy <rmoran20@uic.edu>
2015-05-09 04:55:43 -0500

    modified repair_submission model with validation parameters

--- app/models/repair_submission.rb
+++ app/models/repair_submission.rb
@@ -1,7 +1,21 @@
 class RepairSubmission < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}
+	validates :name, presence: true
 	validates :zip, presence: true
-	#validates(:email, precsence: true)
+	validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX} #removed email unqiueness for submission
+	
+	validates :vehicleYear, presence: true
+	validates :vehicleMake, presence: true
+	validates :vehicleModel, presence: true
+	validates :vehicleTrim, presence: true
+	validates :vehicleMileage, presence: true
+
+	validates :repairDescription, presence: true
+	validates :partsCost, presence: true
+	validates :laborCost, presence: true
+
+	validates :shopName, presence: true
+	validates :shopCityState, presence: true
+	validates :review, presence: true
 end
