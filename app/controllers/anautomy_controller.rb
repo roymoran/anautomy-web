6cc645c83d894494233c4a6f3d19dd5818d5dd27
Roy <rmoran20@uic.edu>
2015-06-20 15:56:20 -0400

    modified user input for both car and repair to always capitalize the first letter of each word. Makes for better display in views

--- app/controllers/anautomy_controller.rb
+++ app/controllers/anautomy_controller.rb
@@ -51,8 +51,12 @@ class AnautomyController < ApplicationController
 
   def repairs
     flash.now[:notice] = "#{$image_link}"
-        @car = $car
-        @repair = $repair
+        @car_lower = $car
+        @car = @car_lower.split.map(&:capitalize).join(' ')
+
+        @repair_lower = $repair
+        @repair = @repair_lower.split.map(&:capitalize).join(' ')
+
         @average_repair_cost = $average_repair_cost
         @average_parts_cost = $average_parts_cost
         @average_labor_cost = $average_labor_cost
@@ -61,8 +65,12 @@ class AnautomyController < ApplicationController
   end
 
   def submissions
-    @car = $car
-    @repair = $repair
+    @car_lower = $car
+    @car = @car_lower.split.map(&:capitalize).join(' ')
+
+    @repair_lower = $repair
+    @repair = @repair_lower.split.map(&:capitalize).join(' ')
+
     @average_repair_cost = $average_repair_cost
     @average_parts_cost = $average_parts_cost
     @average_labor_cost = $average_labor_cost
