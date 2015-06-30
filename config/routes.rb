9aeb6f21bb236c74050619a6a03f1d9600e86a8c
Roy <rmoran20@uic.edu>
2015-06-30 18:43:47 -0400

    made redesign as main root homepage and updated navbar home to point to main root page

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get '/anautomy/homepage'
   get '/anautomy/shop_profile'
   #root 'static_pages#home'
-  root 'anautomy#welcome'
+  #root 'anautomy#welcome'
   #root 'anautomy#homepage'
+  root '/anautomy/homepage'
   #post   'sub'   => 'subscribers#create'
   resources :subscribers
   resources :repair_submissions
