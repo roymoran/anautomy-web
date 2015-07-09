b827a0f751ae73d2530d1cad73d30236a306a732
Roy <rmoran20@uic.edu>
2015-07-08 20:38:37 -0400

    changed names

--- app/controllers/repair_submissions_controller.rb
+++ app/controllers/repair_submissions_controller.rb
@@ -10,7 +10,7 @@ class RepairSubmissionsController < ApplicationController
 	def create
 		@repairSubmission = RepairSubmission.new(repairSubmission_params)  
 		if @repairSubmission.save 
-		SubscriberMailer.repair_submission_email(@repairSubmission).deliver_now
+		#SubscriberMailer.repair_submission_email(@repairSubmission).deliver_now
 		
 		respond_to do |format|
 			#add fallback to html
@@ -29,7 +29,7 @@ class RepairSubmissionsController < ApplicationController
 	private
 
     def repairSubmission_params
-      params.require(:repair_submission).permit(:name, :zip, :email, :vehicleYear, :vehicleMake, :vehicleModel, :vehicleTrim, :vehicleMileage, :repairDescription, :partsCost, :laborCost, :shopName, :shopCityState, :review)
+      params.require(:repair_submission).permit(:name, :zip, :email, :vehicle_year, :vehicle_make, :vehicle_model, :vehicle_trim, :vehicle_mileage, :repair_description, :parts_cost, :labor_cost, :shop_name, :shop_city_state, :review)
     end
 
 end
