c29ffbf2e0722c41522f16724ce90c9ba88c7424
Roy <rmoran20@uic.edu>
2015-10-20 00:25:45 -0500

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
     :address        => 'smtp.mandrillapp.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => "app36147600@heroku.com",
-    :password       => "Hcyb_VFdZNZIgUlkVCuzHw",
+    :user_name      => "MANDRILL_USERNAME",
+    :password       => "MANDRILL_PASSWORD",
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
