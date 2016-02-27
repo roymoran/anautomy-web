b2ad0c0f07594c6f4b7c1f4417031d0f3a2e8b10
Roy <rmoran20@uic.edu>
2016-02-27 16:49:38 -0600

    updated mailer and new subscriber email

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -2,7 +2,7 @@ class SubscriberMailer < ApplicationMailer
 
   def new_subscriber_email(subscriber)
 	@subscriber = subscriber
-    mail to: subscriber.email, subject: "You're On Our List!"
+    mail to: subscriber.email, subject: "Welcome to Anautomy! Stay Tuned for our Early Launch"
   end
 
   def repair_submission_email(repairSubmitter)
