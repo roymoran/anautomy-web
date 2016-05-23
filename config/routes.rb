0fe7bfbb7a117ab0300c53ac88b04ce1de929631
Roy <rmoran20@uic.edu>
2016-05-23 13:33:34 -0500

    removed unused action in anautomy controller and updated routes to remove associated routes of unused actions. Also removed view for unused action.

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
 
   get 'repairs' => 'anautomy#repairs'
   get 'submissions' => 'anautomy#submissions'
-  get '/anautomy/homepage'
-  get '/anautomy/shop_profile'
 
   #root 'static_pages#home'
   #root 'anautomy#welcome'
