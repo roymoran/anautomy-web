9e94ad458b379e7dca7a28772e81cb0e95457e94
Roy <rmoran20@uic.edu>
2016-05-23 00:01:22 -0500

    added cpunter cache option to repar submissions model and removed unused validations

--- app/models/repair_submission.rb
+++ app/models/repair_submission.rb
@@ -1,20 +1,15 @@
 class RepairSubmission < ActiveRecord::Base
-	belongs_to :shop
+	belongs_to :shop, inverse_of: :repair_submissions, counter_cache: true
 	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :name, presence: true, length: {maximum: 50}
-	validates :zip, presence: true, length: {maximum: 10} 
 	validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX} #removed email unqiueness for submission
 	
 	validates :vehicle_year, presence: true, length: {maximum: 255}
-	validates :vehicle_make, presence: true, length: {maximum: 5}
+	validates :vehicle_make, presence: true, length: {maximum: 50}
 	validates :vehicle_model, presence: true, length: {maximum: 50}
 	validates :vehicle_trim, presence: true, length: {maximum: 50}
 
 	validates :repair_description, presence: true
 	validates :parts_cost, presence: true
 	validates :labor_cost, presence: true
-
-	validates :shop_name, presence: true
-	validates :shop_city_state, presence: true, length: {maximum: 50}
 end
