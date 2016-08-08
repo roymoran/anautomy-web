3b5498a878a148f019a6a226a0c7842bde12e4a1
Roy <rmoran20@uic.edu>
2016-08-07 17:44:03 -0700

    updated repair submissions controller to pass shop_id to @repairSubmission object. New view was updated w/ hidden field used to pass shop_id as query string parameter.

--- app/controllers/repair_submissions_controller.rb
+++ app/controllers/repair_submissions_controller.rb
@@ -6,6 +6,7 @@ class RepairSubmissionsController < ApplicationController
 	def new
 		@shop = Shop.find(params[:shop_id])
 		@repairSubmission = RepairSubmission.new
+		@repairSubmission.shop_id = @shop.id
 	end
 
 	def create
@@ -34,7 +35,7 @@ class RepairSubmissionsController < ApplicationController
 	private
 
     def repairSubmission_params
-      params.require(:repair_submission).permit(:email, :vehicle_year, :vehicle_make, :vehicle_model, :vehicle_trim, :vehicle_mileage, :repair_description, :parts_cost, :labor_cost, :total_cost, :review)
+      params.require(:repair_submission).permit(:email, :vehicle_year, :vehicle_make, :vehicle_model, :vehicle_trim, :vehicle_mileage, :repair_description, :parts_cost, :labor_cost, :total_cost, :review, :shop_id)
     end
 
 end
