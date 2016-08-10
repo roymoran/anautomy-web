c46659841774fcff75470f55cdb94d4b3cc9b375
Roy <rmoran20@uic.edu>
2016-08-09 20:48:16 -0700

    updated schema with new active record model for list of all car makes. updated seeds.rb to include seed data of all car makes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523052158) do
+ActiveRecord::Schema.define(version: 20160810012914) do
+
+  create_table "car_makes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "repair_submissions", force: :cascade do |t|
     t.string   "email"
