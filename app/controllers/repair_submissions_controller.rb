85e49e9a6e3d6c0d1ed7b02f3952e59edbf41201
Roy <rmoran20@uic.edu>
2016-08-25 01:26:30 -0700

    added virtual attribute to RepairSubmission model for car_make and car_model. These virtual attribtutes are used to create the associated model with @repairSubmission.create_car(car_make_id: car_make, car_model_id: car_model). The repair_submissions_controller also contains a line to create the car model from the repair_submission object.

--- app/controllers/repair_submissions_controller.rb
+++ app/controllers/repair_submissions_controller.rb
@@ -1,4 +1,5 @@
 class RepairSubmissionsController < ApplicationController
+
 	def show
 		@repairSubmission = RepairSubmission.all
 	end
@@ -10,8 +11,12 @@ class RepairSubmissionsController < ApplicationController
 	end
 
 	def create
-		@repairSubmission = RepairSubmission.new(repairSubmission_params)  
+		@repairSubmission = RepairSubmission.new(repairSubmission_params)
 		if @repairSubmission.save 
+		# @repairSubmission.create_car is used to create and save the associated car to
+		# the repair submission. Params passed by new action is passed as field values  
+		@repairSubmission.create_car(car_make_id: @repairSubmission.car_make, car_model_id: @repairSubmission.car_model)
+
 		SubscriberMailer.repair_submission_email(@repairSubmission).deliver_now
 		
 		respond_to do |format|
@@ -35,7 +40,7 @@ class RepairSubmissionsController < ApplicationController
 	private
 
     def repairSubmission_params
-      params.require(:repair_submission).permit(:email, :vehicle_year, :vehicle_make, :vehicle_model, :vehicle_trim, :vehicle_mileage, :repair_description, :parts_cost, :labor_cost, :total_cost, :review, :shop_id)
+      params.require(:repair_submission).permit(:email, :vehicle_year, :vehicle_trim, :vehicle_mileage, :repair_description, :parts_cost, :labor_cost, :total_cost, :review, :car_make, :car_model, :shop_id)
     end
 
 end
--- app/models/repair_submission.rb
+++ app/models/repair_submission.rb
@@ -1,12 +1,18 @@
 class RepairSubmission < ActiveRecord::Base
+	# attr_accessor added for car_make and car_model (virtual attribute) to save
+	# value of the car_make and car_model option value. This is then sent as a paramter
+	# when create the car in the create action
+	attr_accessor :car_make
+    attr_accessor :car_model
+    
 	belongs_to :shop, inverse_of: :repair_submissions, counter_cache: true
+	has_one :car
+
 	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX} #removed email unqiueness for submission
 	
 	validates :vehicle_year, presence: true, length: {maximum: 255}
-	validates :vehicle_make, presence: true, length: {maximum: 50}
-	validates :vehicle_model, presence: true, length: {maximum: 50}
 	validates :vehicle_trim, presence: true, length: {maximum: 50}
 
 	validates :repair_description, presence: true
