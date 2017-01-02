e05874f30e7ebe7ed5df93920cd5aaeb5faff36d
Roy <rmoran20@uic.edu>
2017-01-01 17:46:36 -0800

    removed @car_owner initilization in all actions for anautomy controller. Instead added before_filter in application controller to initialize @car_owner.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :intialize_car_owner
   protect_from_forgery with: :exception
   attr_accessor :car
   attr_accessor :repair
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
   attr_accessor :image_link
   
   include SessionsHelper
-  
+  @car_owner = CarOwner.new
+
+  def intialize_car_owner
+    @car_owner = CarOwner.new
+  end
 end
