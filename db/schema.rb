b5ad9c1d344527b1dde80145c15844278b72b0cd
Roy <rmoran20@uic.edu>
2017-01-12 16:26:16 -0800

    added email on shops table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110063823) do
+ActiveRecord::Schema.define(version: 20170111195302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -241,6 +241,7 @@ ActiveRecord::Schema.define(version: 20170110063823) do
     t.string   "yelp_contact_date"
     t.string   "register_id"
     t.integer  "shop_users_count",         default: 0
+    t.string   "email"
   end
 
   create_table "subscribers", force: :cascade do |t|
