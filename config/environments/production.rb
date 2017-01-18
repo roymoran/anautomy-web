9eea294b920f81edc5cbdd5f0c9021f9f2dc184f
Roy Moran <roy_moran1993@yahoo.com>
2017-01-18 19:18:02 +0000

    modified production.rb in environment with correct host name and removed modal from header signup and login buttons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'www.innoy.com'
+  host = 'www.innvoy.com'
   config.action_mailer.default_url_options = { host: host }
 
    ActionMailer::Base.smtp_settings = {
