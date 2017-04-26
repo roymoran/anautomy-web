145980e2d6a1c454a7cddb02669e3395d3578fc2
Roy <rmoran20@uic.edu>
2017-04-25 22:39:03 -0600

    added rails migration to store car_owner location data and current_mileage to leverage for maintenance schedule for belonging car.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118042519) do
+ActiveRecord::Schema.define(version: 20170426043536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,6 +155,9 @@ ActiveRecord::Schema.define(version: 20170118042519) do
     t.integer  "cars_count",        default: 0
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
   end
 
   add_index "car_owners", ["email"], name: "index_car_owners_on_email", unique: true, using: :btree
@@ -172,8 +175,9 @@ ActiveRecord::Schema.define(version: 20170118042519) do
     t.datetime "updated_at",           null: false
     t.integer  "repair_submission_id"
     t.integer  "car_year_id"
-    t.integer  "car_owner_id"
     t.integer  "car_option_id"
+    t.integer  "car_owner_id"
+    t.integer  "current_mileage"
   end
 
   add_index "cars", ["car_make_id"], name: "index_cars_on_car_make_id", using: :btree
