a2d3f8a6d959c3dacb212a3966e3dffedafbadbb
Roy <rmoran20@uic.edu>
2017-04-27 18:25:56 -0600

    added blank update action and added current mileage to parameter whitelist

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,9 +6,14 @@ class CarsController < ApplicationController
 		head :created # returning status 201 on creation
 	end
 
+	def update
+		# TODO - update car owner car mileage from user dashboard
+		# Validate that car belongs to current user before updating
+	end
+
 	private
 		def car_params
-			params.require(:car).permit(:car_make_id, :car_model_id, :car_year_id, :car_option_id, :repair_submission_id, :car_owner_id)
+			params.require(:car).permit(:car_make_id, :car_model_id, :car_year_id, :car_option_id, :repair_submission_id, :car_owner_id, :current_mileage)
 		end
 
 	end
