ea8f7e58577e87c39bbb276673f9cdb38ec36ea8
Roy <rmoran20@uic.edu>
2017-11-05 11:28:53 -0700

    include associations and count for ServiceRequest class

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -1,7 +1,8 @@
 class ServiceRequest < ActiveRecord::Base
-  belongs_to :car_owner, inverse_of: :service_requests
+  belongs_to :car_owner, inverse_of: :service_requests, counter_cache: true
   belongs_to :car, inverse_of: :service_requests
   belongs_to :driver, inverse_of: :service_requests
+  belongs_to :shop, inverse_of: :service_requests
   attr_accessor :search_street_number, :search_route, :search_locality, :search_administrative_area_level_1, :search_postal_code, :search_country
 
 
