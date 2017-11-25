3b43d1b0896af656458b4ca71df341f1913576ff
Roy <rmoran20@uic.edu>
2017-11-25 13:29:28 -0800

    add asset to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( jquery.js )
 Rails.application.config.assets.precompile += %w( jquery-ui.js )
+Rails.application.config.assets.precompile += %w( jquery.searcher.min.js )
