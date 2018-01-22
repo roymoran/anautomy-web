8c1fd847e3ea45ae35a08db26fc6dc6261ef1778
Roy Moran <roy_moran1993@yahoo.com>
2018-01-21 19:56:41 -0800

    read sendgrid credentials from production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
    ActionMailer::Base.smtp_settings = {
-    user_name:  '',
-    password: '',
+    user_name:  Rails.application.secrets.sendgrid_username,
+    password: Rails.application.secrets.sendgrid_password,
     domain: 'www.innvoy.com',
     address: 'smtp.sendgrid.net',
     port: 587,
