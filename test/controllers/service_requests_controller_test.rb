8d359100183375006e4f3ff3803569efaaafe499
Roy Moran <roy_moran1993@yahoo.com>
2018-06-10 17:43:43 -0700

    add placeholder assertions for validating stripe charges

--- test/controllers/service_requests_controller_test.rb
+++ test/controllers/service_requests_controller_test.rb
@@ -1,6 +1,25 @@
 require 'test_helper'
 
 class ServiceRequestsControllerTest < ActionController::TestCase
+  # setup do
+  #   @shop = shops(:one)
+  # end
+  
+  test "coupon is valid" do 
+    assert true
+  end
+
+  test "coupon does not exist" do 
+    assert true    
+  end
+
+  test "coupon has already been redeemed" do
+    assert true    
+  end
+
+  test "coupon is expired" do 
+    assert true    
+  end
   # test "should get new" do
   #   get :new
   #   assert_response :success
